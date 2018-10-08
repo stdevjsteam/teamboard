a7a4eb20d96a0ee3ae7c1d226334933ab94dae8e
H <hovhannes.st.dev@gmail.com>
2018-10-08 18:53:04 +0400

    //

--- mobile/index.js
+++ mobile/index.js
@@ -1,6 +1,14 @@
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './src/screens';
 import './debugger';
+// symbol polyfills
+// global.Symbol = require('core-js/es6/symbol');
+// require('core-js/fn/symbol/iterator');
+
+// // collection fn polyfills
+// require('core-js/fn/map');
+// require('core-js/fn/set');
+// require('core-js/fn/array/find');
 
 registerScreens();
 
